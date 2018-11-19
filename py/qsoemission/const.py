5774438ab9332665e9c37419cdf76943e43742b2
JULIANNA STERMER <jstermer@lpnp204.in2p3.fr>
2018-11-19 13:21:44 +0100

    progess with writing in fits file and loop over all pix files

--- py/qsoemission/const.py
+++ py/qsoemission/const.py
@@ -4,3 +4,14 @@ emission_lines = {
     'CIV'   : 1549.48,
     'MgII'  : 2799.117,
     }
+
+line_dict = {
+    'Lya_z' : [],
+    'Lya_sn' : [],
+    'CIII_z' : [],
+    'CIII_sn' : [],
+    'CIV_z' : [],
+    'CIV_sn' : [],
+    'MgII_z' : [],
+    'MgII_sn' : [],
+}
--- py/qsoemission/io.py
+++ py/qsoemission/io.py
@@ -2,11 +2,10 @@ import numpy as np
 import glob
 import fitsio
 
-def read_pix(filename, qso_id):
-    h = fitsio.FITS(filename)
+def read_pix(h, qso_id):
+    ivar = h['IVAR'][:,qso_id].T[0]
     flux =  h['FLUX'][:,qso_id].T[0]
     wave = 10**h['LOGLAM_MAP'][:] # in LF
-    ivar = h['IVAR'][:,qso_id].T[0]
     qso_num = h['THING_ID_MAP'][:][qso_id]
     return flux, ivar, wave, qso_num
 
@@ -17,4 +16,11 @@ def read_drq(drq):
     except:
         z_dict = {x:y for x,y in zip(vac[1]['THING_ID'][:], vac[1]['Z_VI'][:])}    
     return z_dict
+
+def write_fits(out, dic):
+    fits = fitsio.FITS(out,'rw', clobber = True)
     
+    na = list(dic.keys())
+    lis = [np.array(dic[key])  for key in dic.keys()]
+    fits.write(lis, names = na)
+    fits.close()
